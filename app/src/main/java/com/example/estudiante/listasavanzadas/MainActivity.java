c0cfdcd6e37ccaeabdec6d8bb1f9cdbcdd19d991
isabellajordan <isabellajordanbe@gmail.com>
2018-08-31 15:43:36 -0500

    ListaAvanzada semana 5 agregar en tiempo real

--- app/src/main/java/com/example/estudiante/listasavanzadas/MainActivity.java
+++ app/src/main/java/com/example/estudiante/listasavanzadas/MainActivity.java
@@ -4,14 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView lv_noticias;
     //clase noticiaAdapter que tiene un customAdapter
     NoticiaAdapter customAdapter;
+    EditText et_titulo;
+    Button btn_generar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lv_noticias= findViewById(R.id.lv_noticias);
+        et_titulo= findViewById(R.id.et_titulo);
+        btn_generar=findViewById(R.id.btn_generar);
 
         //crear adaptador para personalizar las listas
         customAdapter= new NoticiaAdapter(this);
@@ -50,6 +58,37 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        });
+
+        //agregar noticia
+        btn_generar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //adquirir el título
+                String titulo=et_titulo.getText().toString();
+
+                //traer la fecha actual
+                Calendar c= Calendar.getInstance();
+
+                //sacar los números de fecha
+                int year= c.get(Calendar.YEAR);
+                int month= c.get(Calendar.MONTH);
+                month++;
+                int day= c.get(Calendar.DAY_OF_MONTH);
+
+                String fecha= day+"/"+month+"/"+year+"/";
+
+
+                //generar objeto de noticia
+                Noticia newNoticia= new Noticia(titulo,"NO DESCRIPTION", fecha);
+                customAdapter.agregarNoticia(newNoticia);
+
+
+
+
+
+            }
         });
 
     }
--- app/src/main/java/com/example/estudiante/listasavanzadas/NoticiaAdapter.java
+++ app/src/main/java/com/example/estudiante/listasavanzadas/NoticiaAdapter.java
@@ -1,11 +1,14 @@
 package com.example.estudiante.listasavanzadas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -45,7 +48,7 @@ public class NoticiaAdapter extends BaseAdapter{
 
     //generar un renglón por objeto
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
 
         //Transformar un XML (txt) en una vista (view) para poder retornar
         LayoutInflater inflater = activity.getLayoutInflater();
@@ -59,11 +62,38 @@ public class NoticiaAdapter extends BaseAdapter{
         TextView item_fecha = renglon.findViewById(R.id.item_fecha);
         TextView item_descripcion = renglon.findViewById(R.id.item_descripcion);
 
+        //botón por cada ítem
+        Button item_action= renglon.findViewById(R.id.item_action);
+
         //ubicarlos en la posición
         item_titulo.setText(noticias.get(position).getTitulo());
         item_fecha.setText(noticias.get(position).getFecha());
         item_descripcion.setText(noticias.get(position).getDescripcion());
 
+        //agregar acción para botón para todos los renglones
+item_action.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        //conocer la posición de cada uno de los ítem
+        Toast.makeText(activity, "POS"+ position, Toast.LENGTH_SHORT).show();
+
+        //Eliminar algun ítem de la lista!!
+        noticias.remove(position);
+        notifyDataSetChanged();
+
+        //Ir a otra actividad en este contexto "activity"
+        // contexto y a dónde se quiere ir
+        Intent intent= new Intent(activity,NoticiaView.class);
+        activity.startActivity(intent);
+
+        //Si lo hiciéramos en el main
+        //Intent i= new Intent(MainActivity.this,NoticiaView.class);
+        //starActivity(i);
+
+
+
+    }
+});
 
         return renglon;
     }
